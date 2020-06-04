300bf76809a8348c0b94d11a9187f03a96fe53e9
MiZe1502 <z.com@mail.ru>
2020-06-04 19:28:46 +0300

    added animation to enemy death

--- src/components/Enemy.svelte
+++ src/components/Enemy.svelte
@@ -1,7 +1,9 @@
 <script>
+  import { fade, fly } from 'svelte/transition';
+
   export let enemy;
 </script>
 
-<g transform={`translate(${enemy.x}, ${enemy.y})`}>
+<g out:fade transform={`translate(${enemy.x}, ${enemy.y})`}>
   <rect fill={enemy.color} width={enemy.size} height={enemy.size} />
 </g>
